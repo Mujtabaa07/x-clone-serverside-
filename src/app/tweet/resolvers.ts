3f4ab3f6962dc63edaac9df6d22ffaeabf0888d9
Mohamed Mujtaba <mohamedmujtaba07@gmail.com>
2024-09-24 19:30:32 +0530

    images done

--- src/app/tweet/resolvers.ts
+++ src/app/tweet/resolvers.ts
@@ -1,13 +1,46 @@
 import { Tweet } from "@prisma/client";
-import { prismaClient } from "../../clients/db";
 import { GraphqlContext } from "../../interfaces";
-interface CreateTweetPayload {
-  content: string;
-  imageURL?: string;
-}
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+import UserService from "../../services/user";
+import TweetService, { CreateTweetPayload } from "../../services/tweet";
+
+const s3Client = new S3Client({
+    region: process.env.AWS_DEFAULT_REGION
+  });
+
+
+
 const queries = {
-  getAllTweets: () =>
-    prismaClient.tweet.findMany({ orderBy: { createdAt: "desc" } }),
+  
+    getAllTweets:()=>TweetService.getAllTweets(),
+
+  getSignedURLForTweet: async (
+    parent: any,
+    { imageType, imageName }: { imageType: string; imageName: string },
+    ctx: GraphqlContext
+  ) => {
+    if (!ctx.user || !ctx.user.id) throw new Error("Unauthenticated");
+    const allowedImageTypes = [
+      "image/jpg",
+      "image/jpeg",
+      "image/png",
+      "image/webp",
+    ];
+    if (!allowedImageTypes.includes(imageType))
+      throw new Error("Unsupported Image Type");
+    const putObjectCommand = new PutObjectCommand({
+      Bucket: process.env.AWS_S3_BUCKET,
+      ContentType: imageType,
+      Key: `uploads/${ctx.user.id}/tweets/${imageName}-${Date.now()}`,
+    });
+    const signedURL = await getSignedUrl(s3Client, putObjectCommand);
+    return signedURL;
+  },
+
+
+
 };
 const mutations = {
   createTweet: async (
@@ -16,12 +49,9 @@ const mutations = {
     ctx: GraphqlContext
   ) => {
     if (!ctx.user) throw new Error("You are not authenticated");
-    const tweet = await prismaClient.tweet.create({
-      data: {
-        content: payload.content,
-        imageURL: payload.imageURL,
-        author: { connect: { id: ctx.user.id } },
-      },
+    const tweet = await TweetService.createTweet({
+      ...payload,
+      userId: ctx.user.id,
     });
     return tweet;
   },
@@ -29,7 +59,7 @@ const mutations = {
 const extraResolvers = {
   Tweet: {
     author: (parent: Tweet) =>
-      prismaClient.user.findUnique({ where: { id: parent.authorId } }),
+      UserService.getUserById(parent.authorId),
   },
 };
 export const resolvers = { mutations, extraResolvers, queries };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { initServer } from "./app";
+import * as dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
 async function init() {
   const app = await initServer();
+  app.use(cors());
   app.listen(8000, () => console.log(`Server Started at PORT:8000`));
 }
 init();
